aafec660e65915fded2153ae587b54fd34fbfae7
= <=>
2020-12-11 23:38:29 +0530

    Padding Added, Now images are 28X28 px

--- processing.js
+++ processing.js
@@ -16,13 +16,41 @@ function predictImage() {
 
     cv.threshold(image, image, 175, 255, cv.THRESH_BINARY)
 
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
+    let contours = new cv.MatVector()
+    let hierarchy = new cv.Mat()
     cv.findContours(image, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
 
     let cnt = contours.get(0)
     let rect = cv.boundingRect(cnt)
     image = image.roi(rect)
+
+    var height = image.rows
+    var width = image.cols
+    // tall and narrow image
+    if(height > width){
+        height = 20
+        const scaleFactor = image.rows / height
+        width = Math.round(image.cols / scaleFactor)
+    }// short and narrow image
+    else{
+        width = 20
+        const scaleFactor = image.cols / width
+        height = Math.round(image.rows / scaleFactor)
+    }
+
+    let newSize = new cv.Size(width, height)
+    cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA)
+
+    const LEFT = Math.ceil(4 + (20 - width)/2)
+    const RIGHT = Math.floor(4 + (20 - width)/2)
+    const TOP = Math.ceil(4 + (20 - height)/2)
+    const BOTTOM = Math.floor(4 + (20 - height)/2)
+
+    // console.log(`T: ${TOP} | B: ${BOTTOM} | L: ${LEFT} | R: ${RIGHT}`);
+
+    const BLACK = new cv.Scalar(0, 0, 0, 0)
+    cv.copyMakeBorder(image, image, TOP, BOTTOM, LEFT, RIGHT, cv.BORDER_CONSTANT, BLACK)
+
 
 
     // Test Code
